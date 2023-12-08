c69de691b1167930f5397e3e53ffadd18aa686a6
keni2002 <yerbisuniverses@gmail.com>
2023-12-08 12:14:28 -0500

    un metodo que reduce el codigo migrando a ese metodo y todo lo que conlleva a la par se crea el metodo para obtener una tarea se agrega el campo checked a cada tarea en el metod post add cambios en el manejo de database.json se acerca la seguridad de database.json

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,49 +1,78 @@
-const { Router }= require('express')
+const { Router } = require('express')
 const router = Router()
 const path = require('path')
 const dataController = require('../models/file');
 const fecha = require('../handlers/fecha');
 const datosJson = new dataController();
 
-router.get('/', (req,res) => {
-    console.log(req.body)
+router.get('/', (req, res) => {
     res.json(datosJson.read(req.body))
-    
 })
 
-router.post('/add',(req,res)=>{
+//trying a method
+function esValid(data, user, pass) {
+    for (let i in data) {
+        if (i == user) {
+            if (data[i]["auth"][0].pass == pass) {
+                return true
+            }
+        }
+        break
+    }
+    return false
+}
+router.get('/:id', (req, res) => {
     let data = datosJson.getData();
-    for (let i in data){
-        if(i == req.body.user){
-            if(data[i]["auth"][0].pass == req.body.pass){
+    const { user, pass } = req.body
+    if (esValid(data,user,pass)) {
+        
+        const taskFound = data[user]["data"].find((t)=>{
+            return t.id === parseInt(req.params.id)
+        })
+        if(taskFound) return res.json(data[user]["data"][req.params.id])
+        else return res.status(404).json({message: "error no source"})
+       
+    } else {
+        res.json({message:"error login"})
+    }
+})
+
+
+
+router.post('/add', (req, res) => {
+    let data = datosJson.getData();
+    for (let i in data) {
+        if (i == req.body.user) {
+            if (data[i]["auth"][0].pass == req.body.pass) {
                 //buscar el mayor id
-                let max=0
-                for (let j in data[i]["data"]){
+                let max = 0
+                for (let j in data[i]["data"]) {
                     //v de valor
                     let v = data[i]["data"][j]
-                    if(parseInt(v.id) >= max ) max = v.id
-                    
+                    if (parseInt(v.id) >= max) max = v.id
+
                 }
                 //modificando el json data temporal
                 //defino el array de datas
-                let dArray  = data[req.body.user]["data"]
+                let dArray = data[req.body.user]["data"]
                 //agregar un nuevo elemento apartir de aqui
                 data[req.body.user]["data"][dArray.length] = {
-                    "id": max+1,
+                    "id": max + 1,
                     "title": req.body.title,
                     "description": req.body.description,
                     "date": fecha(),
-                    "date-target": req.body.dateTarget
+                    "date-target": req.body.dateTarget,
+                    "checked": false
                 }
                 //pull fuerte al archivo
-                
+
                 datosJson.save(data)
-                res.json({message: "success"})
+                res.json({ message: "success" })
             }
             break;
         }
     }
-    res.json({message:"USER NOT LOGIN"})
+    res.json({ message: "error login" })
 })
 
 
