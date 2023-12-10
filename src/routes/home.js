bd3f6e1a6cb12ca256f9c94ec7cca7012a8b4829
keni2002 <yerbisuniverses@gmail.com>
2023-12-10 00:41:29 -0500

    adding new 2 methods   1- delete user,   2- delete one task   * fix get by id in taskFound

--- src/routes/home.js
+++ src/routes/home.js
@@ -23,7 +23,8 @@ router.get('/:id', (req, res) => {
         const taskFound = data[user]["data"].find((t) => {
             return t.id === parseInt(req.params.id)
         })
-        if (taskFound) return res.json(data[user]["data"][req.params.id])
+
+        if (taskFound) return res.json(taskFound)
         else return res.status(404).json({ message: "error no source" })
 
     } else {
@@ -50,10 +51,36 @@ router.post('/add', (req, res) => {
             "checked": false
         }
         //pull fuerte al archivo
-        return res.json(datosJson.save(data)) 
+        return res.json(datosJson.save(data))
     }
     return res.json({ message: "error login" })
 })
 
+router.delete('/delete/:id', (req, res) => {
+    let data = datosJson.getData();
+    const { user, pass } = req.body
+    if (esValid(data, user, pass)) {
+        //no uso find porque necesito el index puro del array
+        const elements = data[user]["data"]
 
+        let taskFound = () => {
+            for (let i in elements) {
+                
+                if( elements[parseInt(i)].id == req.params.id){
+                    return true
+                }
+            }
+            return false
+        }
+        
+        if (taskFound()) {
+            data[user]["data"] = data[user]["data"].filter((d)=> d.id != req.params.id )
+            //volcado
+            return res.json(datosJson.save(data))
+        }
+
+        else return res.status(404).json({ message: "error no source" })
+    }
+    return res.json({ message: "error login" })
+})
 module.exports = router;
--- src/routes/login.js
+++ src/routes/login.js
@@ -10,14 +10,14 @@ router.post('/signup', (req, res) => {
     const { user, pass } = req.body;
     let data = datosJson.getData();
     //verifico si existe ya el user
-    const existeUser = Object.keys(data).find((u)=> user === u )
-    if(existeUser) return res.json({message:"error user exists"})
+    const existeUser = Object.keys(data).find((u) => user === u)
+    if (existeUser) return res.json({ message: "error user exists" })
 
     //agregando el usuario
     //generating hard security hash
     const saltRounds = 11
-    const newPass = bcrypt.hashSync(pass,saltRounds);
-    
+    const newPass = bcrypt.hashSync(pass, saltRounds);
+
     data[user] = {
         "auth": [
             {
@@ -42,6 +42,19 @@ router.delete('/:user/flush', (req, res) => {
     const { user, pass } = req.body
     if (esValid(data, user, pass)) {
         data[user]["data"] = {}
+        return res.json(datosJson.save(data))
+    }
+    res.json({ message: "error login" })
+})
+
+//barrer usuarios
+router.delete('/:user/destroy', (req, res) => {
+    let data = datosJson.getData();
+    const { user, pass } = req.body
+    if (esValid(data, user, pass)) {
+        //FORMA PARA ELIMINAR UN ELEMENTO POR LA CLAVE EN JSON
+        delete data[user]
+        
         return res.json(datosJson.save(data))
     }
     res.json({ message: "error login" })
